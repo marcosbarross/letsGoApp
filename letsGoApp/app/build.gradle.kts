fe53c294ef08a6dc143595116f07921c576bffa3
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2024-03-11 10:22:37 -0300

    Adiciona tela de cadastro de pontos de encontro

--- letsGoApp/app/build.gradle.kts
+++ letsGoApp/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("androidx.annotation:annotation:1.7.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- letsGoApp/app/src/main/java/com/example/letsgoapp/ui/dashboard/DashboardFragment.kt
+++ letsGoApp/app/src/main/java/com/example/letsgoapp/ui/dashboard/DashboardFragment.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.letsgoapp.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,16 +17,13 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        // Aqui, use binding.titulo para acessar o TextView com o ID "titulo"
+        val tituloTextView = binding.titulo
+        // Você pode fazer o que quiser com o TextView aqui
+
         return root
     }
 
