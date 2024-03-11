1ce5b4e3e399b0ed09b834486e0a900adb0972c3
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2024-03-11 10:37:53 -0300

    Cria padding no mapa para não sobrepor controles

--- letsGoApp/app/src/main/java/com/example/letsgoapp/ui/home/HomeFragment.kt
+++ letsGoApp/app/src/main/java/com/example/letsgoapp/ui/home/HomeFragment.kt
@@ -4,41 +4,52 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.letsgoapp.R
 import com.example.letsgoapp.databinding.FragmentHomeBinding
-import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
     private lateinit var mapView: MapView
-
+    private lateinit var map: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        mapView = root.findViewById(R.id.mapView)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        mapView = binding.mapView
         mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync { googleMap ->
-        }
+        mapView.getMapAsync(this)
         return root
     }
 
+    override fun onMapReady(googleMap: GoogleMap){
+        map = googleMap
+        val ifpe = LatLng(-7.945590983202563, -34.85879696004683)
+        map.addMarker(MarkerOptions().position(ifpe).title("IFPE").draggable(true))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(ifpe, 13.5f))
+
+        map.uiSettings.apply {
+            isZoomControlsEnabled = true
+            isMyLocationButtonEnabled = true
+        }
+
+        map.setPadding(0,0,0,150)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
     override fun onResume() {
         super.onResume()
         mapView.onResume()
