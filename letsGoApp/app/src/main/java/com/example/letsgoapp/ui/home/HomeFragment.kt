8c4ee1d233efd100b68e9c8b523e6df9a833a486
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2024-03-11 08:00:09 -0300

    Adiciona mapa

--- letsGoApp/app/src/main/java/com/example/letsgoapp/ui/home/HomeFragment.kt
+++ letsGoApp/app/src/main/java/com/example/letsgoapp/ui/home/HomeFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.letsgoapp.R
 import com.example.letsgoapp.databinding.FragmentHomeBinding
+import com.google.android.gms.maps.MapView
 
 class HomeFragment : Fragment() {
 
@@ -16,27 +18,45 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var mapView: MapView
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        mapView = root.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync { googleMap ->
         }
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
 }
