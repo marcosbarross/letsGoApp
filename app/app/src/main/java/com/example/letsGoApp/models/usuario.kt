d65dca8a1f26e348c010ef4de7ac9ed46683fd80
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2024-05-15 10:26:49 -0300

    Adiciona tela de cadastro de usuário

--- app/app/src/main/java/com/example/letsGoApp/models/usuario.kt
+++ app/app/src/main/java/com/example/letsGoApp/models/usuario.kt
@@ -4,11 +4,38 @@ import com.google.gson.annotations.SerializedName
 
 data class usuario(
     @SerializedName("nome")
-    var nome : String,
-    @SerializedName("email")
-    var email : String,
+    val nome: String,
+    @SerializedName("sobrenome")
+    val sobrenome: String,
+    @SerializedName("sexo")
+    val sexo: String,
+    @SerializedName("data_nascimento")
+    val dataNascimento: String,
+    @SerializedName("cep")
+    val cep: String,
+    @SerializedName("endereco")
+    val endereco: String,
+    @SerializedName("cpf")
+    val cpf: String,
     @SerializedName("senha")
-    var senha : String
+    val senha: String,
+    @SerializedName("email")
+    val email: String
+)
+
+data class PontoDeEncontro(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("cep")
+    val cep: String,
+    @SerializedName("rua")
+    val rua: String,
+    @SerializedName("numero")
+    val numero: Int,
+    @SerializedName("complemento")
+    val complemento: String?,
+    @SerializedName("atividade")
+    val atividade: String
 )
 
 data class usuarioAuth(
